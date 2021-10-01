d2b5136857ee53a95f25ce585974b0599ec579e6
Andrew Thomas <ajthomas145@gmail.com>
2021-10-01 17:29:32 +0100

    4th commit

--- console.py
+++ console.py
@@ -8,6 +8,11 @@ import repositories.country_repository as country_repository
 import repositories.theme_park_repository as theme_park_repository
 
 
+country1 = Country("USA", "North America")
+
+attraction1 = Attraction("Space Mountain", "Thrill Ride")
+
+theme_park1 = Theme_park("Walt Disney World", "Space Mountain", "USA")
 
 
 pdb.set_trace()
--- tests/theme_park_test.py
+++ tests/theme_park_test.py
@@ -6,12 +6,14 @@ class TestTheme_park(unittest.TestCase):
     def setUp(self):
         self.theme_park = Theme_park("Walt Disney World", "Space Mountain", "USA")
 
+
     def test_theme_park_has_name(self):
         self.assertEqual("Walt Disney World", self.theme_park.name)
 
+
     def test_theme_park_has_attraction(self):
         self.assertEqual("Space Mountain", self.theme_park.attraction)
-
+        
 
     def test_theme_park_has_country(self):
         self.assertEqual("USA", self.theme_park.country)
