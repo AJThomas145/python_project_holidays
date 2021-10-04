08b3e45e71bd80a8899ae2c5de860c1dd8e87f8b
Andrew Thomas <ajthomas145@gmail.com>
2021-10-04 15:46:16 +0100

    refactor test cases

--- tests/attraction_test.py
+++ tests/attraction_test.py
@@ -4,10 +4,13 @@ from models.attraction import Attraction
 class TestAttraction(unittest.TestCase):
 
     def setUp(self):
-        self.attraction = Attraction("Space Mountain", "Thrill Ride")
+        self.attraction = Attraction("Space Mountain", "Thrill Ride", "Walt Disney World")
 
     def test_attraction_has_name(self):
         self.assertEqual("Space Mountain", self.attraction.name)
 
     def test_attraction_has_category(self):
         self.assertEqual("Thrill Ride", self.attraction.category)
+
+    def test_theme_park_has_theme_park(self):
+        self.assertEqual("Walt Disney World", self.attraction.theme_park)
--- tests/theme_park_test.py
+++ tests/theme_park_test.py
@@ -4,15 +4,11 @@ from models.theme_park import Theme_park
 class TestTheme_park(unittest.TestCase):
 
     def setUp(self):
-        self.theme_park = Theme_park("Walt Disney World", "USA", "Space Mountain")
+        self.theme_park = Theme_park("Walt Disney World", "USA")
 
 
     def test_theme_park_has_name(self):
         self.assertEqual("Walt Disney World", self.theme_park.name)
-
-
-    def test_theme_park_has_attraction(self):
-        self.assertEqual("Space Mountain", self.theme_park.attraction)
 
 
     def test_theme_park_has_country(self):
